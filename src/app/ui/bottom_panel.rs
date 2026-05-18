08b92704be629ba65b428d968c63a5f15d0108fd
Justin Martin <jaming@protonmail.com>
2026-05-18 16:20:48 -0400

    Use a bottom-left anchored foreground area sized to the ui width as .show_inside doesn't render at the top level like now deprecated .show for the bottom panel.  Update vapoursynth and eframe.

--- src/app/ui/bottom_panel.rs
+++ src/app/ui/bottom_panel.rs
@@ -11,55 +11,63 @@ impl UiBottomPanel {
             .get_mut(&pv.state.cur_output)
             .ok_or_else(|| anyhow!("UiBottomPanel::ui: Invalid current output key"))?;
         let node_info = &output.vsoutput.node_info;
+        let width = ui.available_width();
+        egui::Area::new(egui::Id::new("bottom_slider_overlay"))
+            .anchor(egui::Align2::LEFT_BOTTOM, egui::Vec2::ZERO)
+            .order(egui::Order::Foreground)
+            .default_width(width)
+            .default_height(65.0)
+            .show(ui.ctx(), |ui| {
+                let transparent_frame = egui::Frame::default()
+                    .fill(Color32::from_black_alpha(96))
+                    .inner_margin(MarginF32 {
+                        left: 20.0,
+                        right: 20.0,
+                        top: 10.0,
+                        bottom: 10.0,
+                    });
+                ui.set_min_width(width);
+                ui.set_max_width(width);
+                egui::Panel::bottom("BottomInfo")
+                    .frame(transparent_frame)
+                    .show_inside(ui, |ui| {
+                        // Add slider
+                        ui.spacing_mut().slider_width = 600.0;
+                        let mut slider_frame_no = pv.state.cur_frame_no;
 
-        let transparent_frame = egui::Frame::default()
-            .fill(Color32::from_black_alpha(96))
-            .inner_margin(MarginF32 {
-                left: 20.0,
-                right: 20.0,
-                top: 10.0,
-                bottom: 10.0,
-            });
+                        // We want a bit more precision to within ~50 frames
+                        let frames_slider =
+                            egui::Slider::new(&mut slider_frame_no, 0..=(node_info.num_frames - 1))
+                                .smart_aim(false)
+                                .integer();
 
-        egui::Panel::bottom("BottomInfo")
-            .frame(transparent_frame)
-            .show_inside(ui, |ui| {
-                // Add slider
-                ui.spacing_mut().slider_width = 600.0;
+                        let slider_res = ui.add(frames_slider);
+                        let in_use = slider_res.has_focus() || slider_res.drag_started();
+                        let lost_focus = update_input_key_state(
+                            &mut pv.inputs_focused,
+                            "frame_slider",
+                            in_use,
+                            &slider_res,
+                        );
 
-                let mut slider_frame_no = pv.state.cur_frame_no;
+                        // Released/changed value
+                        if lost_focus {
+                            output.last_frame_no = pv.state.cur_frame_no;
+                            pv.state.cur_frame_no = slider_frame_no;
 
-                // We want a bit more precision to within ~50 frames
-                let frames_slider =
-                    egui::Slider::new(&mut slider_frame_no, 0..=(node_info.num_frames - 1))
-                        .smart_aim(false)
-                        .integer();
+                            pv.rerender = true;
+                        } else if slider_frame_no != pv.state.cur_frame_no {
+                            pv.state.cur_frame_no = slider_frame_no;
+                        }
 
-                let slider_res = ui.add(frames_slider);
-                let in_use = slider_res.has_focus() || slider_res.drag_started();
-                let lost_focus = update_input_key_state(
-                    &mut pv.inputs_focused,
-                    "frame_slider",
-                    in_use,
-                    &slider_res,
-                );
+                        let output_info =
+                            format!("Output {} - {}", output.vsoutput.index, node_info);
 
-                // Released/changed value
-                if lost_focus {
-                    output.last_frame_no = pv.state.cur_frame_no;
-                    pv.state.cur_frame_no = slider_frame_no;
-
-                    pv.rerender = true;
-                } else if slider_frame_no != pv.state.cur_frame_no {
-                    pv.state.cur_frame_no = slider_frame_no;
-                }
-
-                let output_info = format!("Output {} - {}", output.vsoutput.index, node_info);
-
-                let node_info_label = egui::RichText::new(output_info)
-                    .color(Color32::from_gray(200))
-                    .size(20.0);
-                ui.label(node_info_label);
+                        let node_info_label = egui::RichText::new(output_info)
+                            .color(Color32::from_gray(200))
+                            .size(20.0);
+                        ui.label(node_info_label);
+                    });
             });
 
         Ok(())
